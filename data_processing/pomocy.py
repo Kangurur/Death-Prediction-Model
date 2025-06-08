a22c1afd2c02efaf9d4d2c833d546910b5d486d0
Kangurur <olipol2005@gmail.com>
2025-06-09 00:05:09 +0200

    ekgPrawieDziała

--- data_processing/pomocy.py
+++ data_processing/pomocy.py
@@ -6,14 +6,20 @@ import json
 # --- 1. Przetwarzanie sygnału EKG ---
 with open('data/digitized_json_files/19_2020.json', 'r') as f:
     raw = json.load(f)
-lead= raw['leads'][0]['signal'][1:]  # Wybierz pierwszy lead
+lead= raw['leads'][0]['signal']  # Wybierz pierwszy lead
+lead[0]=lead[1]
 
 ekg_signal = np.array(lead, dtype=np.float32)  # <-- Twoje dane
 sampling_rate = 320  # Hz
 
 #signals, info = nk.ecg_process(ekg_signal, sampling_rate=sampling_rate)
 
-cleaned_signal = nk.ecg_clean(ekg_signal, sampling_rate=sampling_rate, method="neurokit")
-rpeaks = nk.ecg_peaks(cleaned_signal, sampling_rate=sampling_rate)
+#cleaned_signal = nk.ecg_clean(ekg_signal, sampling_rate=sampling_rate, method="neurokit")
+#rpeaks = nk.ecg_peaks(cleaned_signal, sampling_rate=sampling_rate)
 
-quality = nk.ecg_quality(cleaned_signal, rpeaks=rpeaks, sampling_rate=sampling_rate)
+#quality = nk.ecg_quality(cleaned_signal, sampling_rate=sampling_rate)
+
+signals, info = nk.ecg_process(ekg_signal, sampling_rate=sampling_rate)
+#analysis = nk.ecg_analyze(signals, sampling_rate=sampling_rate, method="interval-related")
+
+#analysis.to_csv('data_processing/analysis_test.csv')
